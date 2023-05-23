3cc0bf62dedb39143017207e898ac206358f123c
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-05-23 15:06:13 -0300

    Adicionado configuracao adicional de configuracoes entre ambientes

--- src/main/java/br/com/haroldo/vendas/MainConfiguration.java
+++ src/main/java/br/com/haroldo/vendas/MainConfiguration.java
@@ -1,8 +1,19 @@
 package br.com.haroldo.vendas;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MainConfiguration {
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO NA CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 
 }
